7ad9d1017c01337967dadbf37665fa1831d390a6
Luis M <lmurallesp@miumg.edu.gt>
2025-09-01 21:34:35 -0600

    feat: add react-router-dom setup with example routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Header from "./components/Header";
+import { useRoutes, BrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home";
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+
+const AppRoutes = () => {
+  
+  const routes = useRoutes([
+    { path: '/', element: <Home />},
+    { path: '/home', element: <Home />},
+    { path: '/about', element: <About />},
+    { path: '*', element: <NotFound />},
+  ])
+  return routes;
+}
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    
   );
 }
