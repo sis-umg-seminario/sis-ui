880b51706838ecc6d99e6f6b0a5e94f4a4ee568d
Luis M <lmurallesp@miumg.edu.gt>
2025-09-01 20:06:31 -0600

    feat: setup tailwindcss and shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <div className="flex flex-col gap-3 min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Vite + Tailwind + TS
+      </h1>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
