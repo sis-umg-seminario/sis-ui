65abfebc77bb68dc3d0554b65f206ab3fa38eb56
lazarof1 <blazarof1@miumg.edu.gt>
2025-09-13 07:37:48 -0600

    Create inscriptions payments

---
+++ src/hooks/payment/useGetEnrollmentFee.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { paymentService } from "@/services/paymentService";
+import type { EnrollmentFeeResponse } from "@/types/payment";
+
+export function useGetEnrollmentFee() {
+  const [enrollmentFee, setEnrollmentFee] = useState<EnrollmentFeeResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const findEnrollmentFee = async (studentId: string) => {
+    setLoading(true);
+    setError(null);
+    setEnrollmentFee(null); 
+    try {
+
+      const data = await paymentService.getEnrollmentFee(studentId, 10);
+      setEnrollmentFee(data);
+    } catch (err) {
+      setError("Error: No se pudo consultar el monto a pagar. Verifique el carné.");
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  return { enrollmentFee, loading, error, findEnrollmentFee };
+}
---
+++ src/services/paymentService.ts
@@ -0,0 +1,29 @@
+import { apiClient } from "./apiClient";
+import {
+  type PaymentTypesResponse,
+  type EnrollmentFeeResponse,
+  type ProcessPaymentPayload,
+  type ProcessPaymentResponse
+} from "@/types/payment";
+
+export const paymentService = {
+
+  getPaymentTypes: () => 
+    apiClient<PaymentTypesResponse>("/payments/paymentTypes"),
+
+
+  getEnrollmentFee: (studentId: string, paymentTypeId: number) =>
+    apiClient<EnrollmentFeeResponse>("/payments/paymentFee", {
+      params: {
+        studentId,
+        paymentTypeId,
+      }
+    }),
+  
+
+  processPayment: (payload: ProcessPaymentPayload) =>
+    apiClient<ProcessPaymentResponse>("/payments/processPayment", {
+      method: "POST",
+      body: JSON.stringify(payload)
+    })
+};
