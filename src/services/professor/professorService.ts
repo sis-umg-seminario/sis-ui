ae0f17cecc67790b2993b398ae4e94732836aebe
Luis M <lmurallesp@miumg.edu.gt>
2025-11-15 01:31:08 -0600

    Professor grades

--- src/services/professor/professorService.ts
+++ src/services/professor/professorService.ts
@@ -1,11 +1,21 @@
 import { apiClient } from "../apiClient";
 import type { AssignedCourses } from "../../types/professor/assignedCourses";
-import type { StudentGrades } from "../../types/professor/studentGrades";
+import type { StudentGrades, GradesToUpdate } from "../../types/professor/studentGrades";
 import type { CourseStudents } from "../../types/professor/courseStudents";
 
-
 export const professorService = {
-  getStudentGrades: (courseOfferingId: number) => apiClient<StudentGrades>(`/professor/course/${courseOfferingId}/student-grades`),
-  getCourseStudents: (courseOfferingId: number) => apiClient<CourseStudents>(`/professor/course/${courseOfferingId}/students`),
-  getAssignedCourses: (professorId: number) => apiClient<AssignedCourses>(`/professor/assigned-courses?professorId=${professorId}`),
+  getStudentGrades: (courseOfferingId: number) =>
+    apiClient<StudentGrades>(`/professor/course/${courseOfferingId}/student-grades`),
+
+  getCourseStudents: (courseOfferingId: number) =>
+    apiClient<CourseStudents>(`/professor/course/${courseOfferingId}/students`),
+
+  getAssignedCourses: (professorId: number) =>
+    apiClient<AssignedCourses>(`/professor/assigned-courses?professorId=${professorId}`),
+
+  updateGrades: (studentId: number, courseOfferingId: number, grades: GradesToUpdate) =>
+    apiClient<void>(`/professor/course/${courseOfferingId}/student-grades/${studentId}`, {
+      method: "PATCH",
+      body: JSON.stringify(grades),
+    }),
 };
